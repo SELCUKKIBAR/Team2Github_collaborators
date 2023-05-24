015e4514be5f79e90233259e3dd02bbde29db1b1
SELCUKKIBAR <selcukkibar88@gmail.com>
2023-05-25 00:18:04 +0300

    first commit

---
+++ src/test/java/C01_main.java
@@ -0,0 +1,12 @@
+public class C01_main {
+    public static void main(String[] args) {
+        /*
+        git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/SELCUKKIBAR/Team2Github_collaborators.git
+git push -u origin main
+         */
+    }
+}
